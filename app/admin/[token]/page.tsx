79771d5ad8cacc16516f17aa17dcb637c0833675
jonasbqz <josebanquez100@gmail.com>
2026-06-09 23:22:10 +0200

    feat(admin): finalize token validation with clean notFound response

--- app/admin/[token]/page.tsx
+++ app/admin/[token]/page.tsx
@@ -13,16 +13,8 @@ export default async function AdminPage({ params }: PageProps) {
     process.env.ADMIN_TOKEN ||
     "control-secreto-2026";
 
-  console.log("DEBUG ADMIN ROUTE:", { token, adminToken });
-
   if (token !== adminToken) {
-    return (
-      <div style={{ padding: "100px", color: "white", background: "black" }}>
-        <h1>DEBUG ADMIN TOKEN</h1>
-        <p>token recibido: &quot;{token}&quot;</p>
-        <p>token esperado: &quot;{adminToken}&quot;</p>
-      </div>
-    );
+    notFound();
   }
 
   return <AdminClient />;
