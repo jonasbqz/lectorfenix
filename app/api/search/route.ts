9c60204fdce750b5560c46cf6994f1e111fe069b
jonasbqz <josebanquez100@gmail.com>
2026-07-02 05:00:06 +0200

    fix: resolve correct relative path for postgres client import in search api

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { logger } from "../../utils/logger";
 import { fetchMangaVfAPI } from "../../utils/monline";
-import { sql } from "../../utils/postgres/client";
+import { sql } from "../../../utils/postgres/client";
 
 export const dynamic = "force-dynamic";
 
